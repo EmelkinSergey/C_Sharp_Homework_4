60fe0d2357bb787545b8b24599bfb8e1970cc535
Sergey Emelkin <sergeymx440@mail.ru>
2023-01-16 19:37:47 +0400

    Exam_029

---
+++ Exam_029/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+
+
+int[] array = NewArray(8);
+Console.WriteLine($" [ {String.Join(", ", array)} ] ");
+
+
+int[] NewArray(int size)
+{
+int[] num = new int[size];
+for (int i = 0; i < size; i++)
+{
+num[i] = new Random().Next(0,100);
+}
+
+return num;
+}
