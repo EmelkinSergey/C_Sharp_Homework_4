d745553761814a47318b3a3a1d2f92a404d2e9c0
Sergey Emelkin <sergeymx440@mail.ru>
2023-01-16 15:23:59 +0400

    ChangeExam_027

--- Exam_027/Program.cs
+++ Exam_027/Program.cs
@@ -7,10 +7,11 @@ Console.WriteLine($"Сумма цифр числа {num} равно {SumNum(num)
 int SumNum (int number)
 {
     int sum = 0;
-    while (number % 10 > 0)
+    while (number / 10 > 0)
     {
-        sum = sum + number % 10;
+        sum = sum + (number % 10);
         number = number / 10;
     }
+    sum = sum + number % 10;
 return sum;
 }
